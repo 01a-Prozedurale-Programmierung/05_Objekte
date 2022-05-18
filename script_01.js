f1b6a55fdd0c0e477851e71cc0106e1ac48d9ffe
Peter <schmidt.multimedia@gmail.com>
2022-05-18 14:40:06 +0200

    Objekt I / Basics

--- script_01.js
+++ script_01.js
@@ -8,10 +8,31 @@
 // output("------------");
 
 
-
 /***** Objekte 1 Daten/Funktionen *******/
 
+let person =    {
+                     firstName: "Yahya",
+                     familyName: "Tetik",
+                     salary: [120000,160000],
+                     permission: true,
+                     sayHello: 
+                     function (){
+                       return "Hello, " +  this.firstName;      
+                     }
+                };
 
+
+// output(person);
+// output(person.firstName);
+// output(person.permission);
+
+const txt =     "Ich bin " + person.firstName + " " +
+                person.familyName  + " und verdiene " +
+                person.salary[0] + " p.a" +  
+                ".";
+
+// output(txt);
+// output(person.sayHello());
 
 
 /***** Objekte 2 - Hierarchie *******/
